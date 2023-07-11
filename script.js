03d771db49c75fd0af9e37e86e9c57ad0029bf50
manish tiwari <manishtiwari769@gmail.com>
2023-07-12 03:34:43 +0530

    conflict not resolved yet

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
-// Create Dynamic Form
+// // Create Dynamic Form
 
-const addBtn = document.querySelector(".add");
+// const addBtn = document.querySelector(".add");
 
-const input = document.querySelector(".inp-group");
+// const input = document.querySelector(".inp-group");
 
-function removeInput(){
-    this.parentElement.remove();
+// function removeInput(){
+//     this.parentElement.remove();
+// }
+
+// function addInput(){
+
+//     const item = document.createElement("input");
+//     item.type = "text";
+//     item.placeholder = "Enter the Item Name";
+
+//     const rate = document.createElement("input");
+//     rate.type = "Number";
+//     rate.placeholder = "Enter the Rate";
+
+//     const qty = document.createElement("input");
+//     qty.type = "Number";
+//     qty.placeholder = "Enter the Quantity";
+
+//     const amount = document.createElement("input");
+//     amount.type = "Number";
+//     amount.placeholder = "Enter the Amount";
+
+//     const btn = document.createElement("a");
+//     btn.className = "delete";
+//     btn.innerHTML = "&times";
+//     btn.addEventListener("click", removeInput);
+
+//     const flex = document.createElement("div");
+//     flex.className = "flex";
+
+//     input.appendChild(flex);
+//     flex.appendChild(item);
+//     flex.appendChild(rate);
+//     flex.appendChild(qty);
+//     flex.appendChild(amount);
+//     flex.appendChild(btn);
+// }
+
+
+// addBtn.addEventListener("click", addInput);
+
+// btn.addEventListener("click", removeInput);
+
+
+function myFunction(){
+    const node = document.getElementsByClassName("index-1");
+    const clone = node.cloneNode(true);
+    document.getElementsByClassName("addInputSection").appendChild(clone);
 }
-
-function addInput(){
-
-    const item = document.createElement("input");
-    item.type = "text";
-    item.placeholder = "Enter the Item Name";
-
-    const rate = document.createElement("input");
-    rate.type = "Number";
-    rate.placeholder = "Enter the Rate";
-
-    const qty = document.createElement("input");
-    qty.type = "Number";
-    qty.placeholder = "Enter the Quantity";
-
-    const amount = document.createElement("input");
-    amount.type = "Number";
-    amount.placeholder = "Enter the Amount";
-
-    const btn = document.createElement("a");
-    btn.className = "delete";
-    btn.innerHTML = "&times";
-    btn.addEventListener("click", removeInput);
-
-    const flex = document.createElement("div");
-    flex.className = "flex";
-
-    input.appendChild(flex);
-    flex.appendChild(item);
-    flex.appendChild(rate);
-    flex.appendChild(qty);
-    flex.appendChild(amount);
-    flex.appendChild(btn);
-}
-
-
-addBtn.addEventListener("click", addInput);
-
-btn.addEventListener("click", removeInput);
