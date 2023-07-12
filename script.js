4d061fb1a8b569a40f114780f5c0541e2dac7a18
Vipul <vipcodeerror@gmail.com>
2023-07-12 23:41:27 +0530

    commits

--- script.js
+++ script.js
@@ -1,56 +1,20 @@
-// // Create Dynamic Form
+function myFunction() {
+    var flag =0;
 
-// const addBtn = document.querySelector(".add");
+    document.getElementById("id_add").addEventListener("click", function (event) {
+     
+        //
+        const target = document.getElementById("index");
+        const copyElement = target.cloneNode(true);
+        copyElement.setAttribute("id", "index-" + (flag + 1))
+        document.getElementById("addInputSection").appendChild(copyElement);
+        flag++;
+        event.preventDefault();
+    });
 
-// const input = document.querySelector(".inp-group");
-
-// function removeInput(){
-//     this.parentElement.remove();
-// }
-
-// function addInput(){
-
-//     const item = document.createElement("input");
-//     item.type = "text";
-//     item.placeholder = "Enter the Item Name";
-
-//     const rate = document.createElement("input");
-//     rate.type = "Number";
-//     rate.placeholder = "Enter the Rate";
-
-//     const qty = document.createElement("input");
-//     qty.type = "Number";
-//     qty.placeholder = "Enter the Quantity";
-
-//     const amount = document.createElement("input");
-//     amount.type = "Number";
-//     amount.placeholder = "Enter the Amount";
-
-//     const btn = document.createElement("a");
-//     btn.className = "delete";
-//     btn.innerHTML = "&times";
-//     btn.addEventListener("click", removeInput);
-
-//     const flex = document.createElement("div");
-//     flex.className = "flex";
-
-//     input.appendChild(flex);
-//     flex.appendChild(item);
-//     flex.appendChild(rate);
-//     flex.appendChild(qty);
-//     flex.appendChild(amount);
-//     flex.appendChild(btn);
-// }
-
-
-// addBtn.addEventListener("click", addInput);
-
-// btn.addEventListener("click", removeInput);
-
-
-function myFunction(){
-    debugger
-    const target = document.getElementById("index-1");
-    const copyElement = target.cloneNode(true);
-    document.getElementsById("addInputSection").appendChild(copyElement);
+    // remove row
+    
 }
+
+
+myFunction();
