0e71331bb429c5a0837b8b01463e87731e94fb24
manish tiwari <manishtiwari769@gmail.com>
2023-07-14 22:25:51 +0530

    still working

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ function deleteRow(rowElementIndex){
 }
 
 myFunction();
+
+
+function findTotal(){
+    var arr = document.getElementsByClassName('amount');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseFloat(arr[i].value))
+            tot += parseFloat(arr[i].value);
+    }
+    document.getElementById('subtotal').value = "$ "+tot;
+}
